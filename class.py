1669f17bafaab9c34dd5bb558c0fcdc5002853cf
Ana Filipa <98351447+filipatrabucho@users.noreply.github.com>
2024-01-26 13:18:12 +0000

    fix Class Dono

--- class.py
+++ class.py
@@ -1,5 +1,5 @@
 class Animal:
-    def __int__(self,id, nome, idade,raca,porte,peso,nr_chip):
+    def __init__(self,id, nome, idade,raca,porte,peso,nr_chip):
         self.id=id
         self.nome=nome
         self.idade=idade
@@ -18,19 +18,23 @@ class Consulta(Animal):
         self.internamento=internamento
     
 class Dono(Animal):
-    def __init__(self,id,nome,contato,nif,morada):
+    def __init__(self,id,nome1,contato,nif,morada):
         super().__init__(id)
-        self.nome=nome
+        self.nome1=nome1
         self.contato=contato
         self.nif=nif
         self.morada=morada
 
     def Imprime(self):
-        print(f"Nome do dono: {self.nome}")
+        print(f"Nome do dono: {self.nome1}")
 
 def main():
-    a2=Dono("1","Ana","987456123","123456789", "Rua Teste 23123")
-    a2.Imprime()
+    cao=Animal(1,"Bobi",5,"Rafeiro","Medio",15,123)
+    cao.InfoAnimal()
 
-
-main()
+    pessoa=Dono(1,"Ana",987456321,123654987,"Rua Teste")
+    pessoa.Imprime()
+  
+   
+if __name__=="__main__": 
+    main()
